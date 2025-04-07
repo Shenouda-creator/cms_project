bf4d0d3f6a06c3d24d5c2a534c87e74f74dcf1be
Shenouda-creator <shenoudamouris602@gmail.com>
2025-04-07 13:12:30 +0200

    auth

---
+++ CMS_Project/routes/backend.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+/*
+|--------------------------------------------------------------------------
+| backend Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::group(
+//     ['prefix' => LaravelLocalization::setLocale()],
+//     function () {
+
+
+//     }
+// );
+
